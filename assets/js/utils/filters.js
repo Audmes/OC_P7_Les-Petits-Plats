20f0d388550900a57648226f671ed2dd6b2b0eb7
Audmes <102157665+Audmes@users.noreply.github.com>
2024-01-23 14:50:21 +0100

    Commit : 23/01/2024 - Ajout des filtres : Work in progress

--- assets/js/utils/filters.js
+++ assets/js/utils/filters.js
@@ -1,3 +1,50 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+// let filtersBtn = document.querySelectorAll('.dropbtn'); 
+
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+//   }
+  
+  // function filterFunction() {
+  //   let input, filter, ul, li, a, i;
+  //   input = document.getElementById("myInput");
+  //   filter = input.value.toUpperCase();
+  //   div = document.getElementById("myDropdown");
+  //   a = div.getElementsByTagName("a");
+
+  //   for (i = 0; i < a.length; i++) {
+  //     txtValue = a[i].textContent || a[i].innerText;
+  //     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+  //       a[i].style.display = "";
+  //     } else {
+  //       a[i].style.display = "none";
+  //     }
+  //   }
+  // }
+
+// let dropInput = document.querySelectorAll(".dropInput");
+
+// function filterFunction() {
+//           let ul, li, i;
+//           let input = document.querySelector(".dropInput");
+//           let filter = input.value.toUpperCase();
+//           let div = document.querySelector(".dropdown-content");
+//           let a = div.querySelector("a");
+
+//           for (i = 0; i < a.length; i++) {
+//             txtValue = a[i].textContent || a[i].innerText;
+//             if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//               a[i].style.display = "";
+//             } else {
+//               a[i].style.display = "none";
+//             }
+//           }
+// }
+
+// dropInput.forEach((btn) => btn.addEventListener("keyup", filterFunction));
+// filtersBtn.forEach((btn) => btn.addEventListener("click", myFunction));
+
 // Multiple Dropdown Menu
 let dropdown = document.querySelectorAll('.dropdown');
 let dropdownArray = Array.prototype.slice.call(dropdown,0);
