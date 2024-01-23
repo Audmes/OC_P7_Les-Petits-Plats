95be8c9b73fd94b9eef411c6f038f463207d0901
Audmes <102157665+Audmes@users.noreply.github.com>
2024-01-23 14:36:19 +0100

    Commit : 23/01/2024 - Ajout de counter JS : compteur du total de recettes

---
+++ assets/js/utils/counters.js
@@ -0,0 +1,21 @@
+// Affichage du total des Recettes
+export const displayTotalRecipes = recipes => {
+    const totalSection = document.querySelector('.filters__title');
+    totalSection.innerHTML = '';
+
+    const totalRecipes = recipes.length;
+    const h2 = document.createElement('h2');
+
+    if (totalRecipes < 2 ) {
+        h2.innerHTML = `
+            `+totalRecipes+` recette
+        `;
+
+    }else {
+        h2.innerHTML = `
+            `+totalRecipes+` recettes
+        `;
+    }
+
+    return totalSection.appendChild(h2);
+}
