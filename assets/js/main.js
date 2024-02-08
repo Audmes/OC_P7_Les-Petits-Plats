0fe786badd6437e2761690f34bc82e1692601875
Audmes <102157665+Audmes@users.noreply.github.com>
2024-02-08 16:19:31 +0100

    Commit : 08/02/2024 - Ajout de la fonctionnalité Tags : Work in progress - Amélioration des filtres

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ import { displayRecipes } from './templates/recipes.js';
 import { searchBar } from './utils/searchbar.js';
 import { displayTotalRecipes } from './utils/counters.js';
 import { displayfilters } from './templates/filters.js';
+import { displayTags } from './templates/tags.js';
 
 const displayPage = async () => {
     // SearchBar
@@ -11,6 +12,9 @@ const displayPage = async () => {
     
     // Display Filters
     displayfilters(recipes);
+
+    // Display Tags
+    displayTags(recipes);
     
     // Display Recipes
     displayRecipes(recipes);
--- assets/js/templates/filters.js
+++ assets/js/templates/filters.js
@@ -10,7 +10,6 @@ export const displayfilters = recipes => {
     const appliancesList = [];
     const ustensilsList = [];
 
-    // On vide les listes à chaque fois que l'on appel la fonction.
     ingredientsBloc.innerHTML = '';
     appliancesBloc.innerHTML = '';
     ustensilsBloc.innerHTML = '';
@@ -19,24 +18,26 @@ export const displayfilters = recipes => {
         /** Ingredients **/
         // if tags already used, don't push it.
         const itags = [...document.querySelectorAll('.tag__ingredient')].map( (itag) => itag.innerText);
+        
         recipe.ingredients.forEach(({ ingredient }) => {
-        if (ingredientsList.includes(ingredient) === false && itags.includes(ingredient) === false) {
-            ingredientsList.push(ingredient);
-            // const filterItem = document.createElement('li');
-            const filterItem = document.createElement('a');
-            filterItem.classList.add('filter__ingredients--items');
-            filterItem.innerText = ingredient;
-            ingredientsBloc.appendChild(filterItem);
-        }
+            let ingredientRegex = ingredient.trim().toLowerCase();
+
+            if (ingredientsList.includes(ingredientRegex) === false && itags.includes(ingredientRegex) === false) {
+                ingredientsList.push(ingredientRegex);
+                const filterItem = document.createElement('li');
+                filterItem.classList.add('filter__ingredients--items');
+                filterItem.innerText = ingredient;
+                ingredientsBloc.appendChild(filterItem);
+            }
         });
 
         /** appliances **/
         // if tags already used, don't push it.
         const atags = [...document.querySelectorAll('.tag__appliance')].map( (atag) => atag.innerText);
+
         if (appliancesList.includes(recipe.appliance) === false && atags.includes(recipe.appliance) === false) {
             appliancesList.push(recipe.appliance);
-            // const filterItem = document.createElement('li');
-            const filterItem = document.createElement('a');
+            const filterItem = document.createElement('li');
             filterItem.classList.add('filter__appliances--items');
             filterItem.innerText = recipe.appliance;
             appliancesBloc.appendChild(filterItem);
@@ -48,8 +49,7 @@ export const displayfilters = recipes => {
         recipe.ustensils.forEach((ustensil) => {
         if (ustensilsList.includes(ustensil) === false && utags.includes(ustensil) === false) {
             ustensilsList.push(ustensil);
-            // const filterItem = document.createElement('li');
-            const filterItem = document.createElement('a');
+            const filterItem = document.createElement('li');
             filterItem.classList.add('filter__ustensils--items');
             filterItem.innerText = ustensil;
             ustensilsBloc.appendChild(filterItem);
